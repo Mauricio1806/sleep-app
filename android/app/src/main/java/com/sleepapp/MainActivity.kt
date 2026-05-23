838289852c6a199493c1dec5b1553b0c0483e862
Mauricio Esquivel <mauricio.esquivel1806@gmail.com>
2026-05-22 22:14:56 -0300

    fix: MainActivity getMainComponentName SleepApp -> Sona

--- android/app/src/main/java/com/sleepapp/MainActivity.kt
+++ android/app/src/main/java/com/sleepapp/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "SleepApp"
+  override fun getMainComponentName(): String = "Sona"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
